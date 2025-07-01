9eaae12ddc85b872b725dd34afa68315d20b2d51
ibrahimkeyboad <91762345+ibrahimkeyboad@users.noreply.github.com>
2025-07-01 14:59:05 +0300

    Updated supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl ="https://xeubklnzrysyxolndwzf.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhldWJrbG56cnlzeXhvbG5kd3pmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEyMDk5MDUsImV4cCI6MjA2Njc4NTkwNX0.IYD2PjC3bEeEOKmS0zXXPDWEfzd0-t5TOmzBG8NP2XA"
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing Supabase environment variables. Please check your .env file.');
